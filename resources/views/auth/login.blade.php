d6ddeb91e4218f5441726edfbf43fec2352f03c0
Frederick A. Gonzalez <g7.alexander@gmail.com>
2019-01-13 11:56:25 -0400

    Login UI Advances

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<b-container>
+    <b-row class="justify-content-center">
+        <b-col cols="8">
+            <b-card title="Inicio de sesión">
+                <b-alert show variant="secondary">
+                    Por favor ingresa tus datos
+                </b-alert>
+                <b-form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <b-form-group 
+                        label="Correo Electronico" label-for="email"
+                        description="Nunca compartiremos tu correo. Está seguro con nosotros.">
+
+                        <b-form-input type="email"
+                            id="email" name="email"
+                            value="{{ old('email') }}" required autofocus
+                            placeholder="example@example.com">
+                        </b-form-input>
+                    </b-form-group>
+
+                    <b-form-group label="Contraseña" label-for="password">
+
+                        <b-form-input type="password"
+                            id="password" name="password"
+                            value="{{ old('password') }}" required autofocus>
+                        </b-form-input>
+                    </b-form-group>
+                    
+                    <b-form-group>
+                        <b-form-checkbox name="remember" {{ old('remember') ? 'checked="true"' : '' }}>
+                            Recordar sesión
+                        </b-form-checkbox>
+                    </b-form-group>
+
+                    <b-button type="submit" variant="primary">
+                        Ingresar
+                    </b-button>
+                    <b-button href="{{ route('password.request') }}" variant="link">
+                        ¿Olvidaste tu contraseña?
+                    </b-button>
+
+                </b-form>
+            </b-card>
+        </b-col>
+    </b-row>
+</b-container>
+@endsection
